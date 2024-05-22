b6ee959cc11092a7af0139ed8c9194716509fddd
Jaroslav Kusák <jarekusak@seznam.cz>
2024-05-22 12:47:03 +0200

    přepsání funkce select na jednodušší verzi

--- SQL_interpreter.hs
+++ SQL_interpreter.hs
@@ -12,7 +12,6 @@ type Row = [Value] -- hodnoty v jednom radku
 -- druha je taky dvojice, seznam sloupcu s nazvy a druha slozka je seznam radku - zaznamu
 -- type Database = [("People", (["ID", "Name"], [["1", "Alice"], ["2", "Bob"], ["3", "Charlie"]]))]
 type Database = [(TableName, (ColumnList, [Row]))] -- databaze obsahuje tabulky a radky
-
 data SQLStatement
     = CreateTable TableName [ColumnName]
     | Insert TableName [Value]
@@ -21,6 +20,10 @@ data SQLStatement
 
 data Join
     = InnerJoin TableName TableName ColumnName ColumnName -- join na zaklade stejnych sloupcu
+
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
 
 createTable :: Database -> TableName -> [ColumnName] -> Database
 createTable db name columns = trace ("Creating table " ++ name ++ " with columns " ++ show columns) $ (name, (columns, [])) : db -- pripoji se k existujicim tabulkam
@@ -32,10 +35,11 @@ insertInto db tableName values = trace ("Inserting into " ++ tableName ++ " valu
       | name == tableName = (name, (columns, values : rows)) -- prida novy radek (zaznam) do pozadovane tabulky
       | otherwise = (name, (columns, rows)) -- ostatni tabulky nech nezmenene
 
+{-
 select :: Database -> [ColumnName] -> TableName -> Maybe Join -> [[Value]]
 select db columns tableName Nothing =
     trace ("Selecting columns " ++ show columns ++ " from table " ++ tableName) $
-    case lookup tableName db of -- vyhledame pozadovanou tabulku v databazi
+    case lookup tableName db of -- vyhledame pozadovanou tabulku v databazi (lookup vrací pro daný klíč - tableName - hodnotu - tabulku (ColumnList, [Row]))
         Nothing -> error $ "Table " ++ tableName ++ " not found." -- neexistuje
         Just (tableCols, rows) ->
             let filteredRows = map (filterColumns columns tableCols) rows
@@ -43,20 +47,45 @@ select db columns tableName Nothing =
   where
     filterColumns cols tableCols row = [value | (col, value) <- zip tableCols row, col `elem` cols]
 
+
+zip ["id", "name", "age", "email"] [1, "John Doe", 30, "john@example.com"] 
+se stane [("id", 1), ("name", "John Doe"), ("age", 30), ("email", "john@example.com")].
+
+filterColumns tedy prochází každý řádek tabulky a vybírá pouze ty hodnoty, které odpovídají požadovaným sloupcům
+filterRows v postatě aplikuje filterColumns na všechny řádky
+-}
+
+select :: Database -> [ColumnName] -> TableName -> Maybe Join -> [[Value]]
+select db columns tableName Nothing = -- columns jsou požadované sloupečky
+    case lookup tableName db of
+        Nothing -> error "error"
+        Just (tableColumns, rows) ->
+            filterRows columns tableColumns rows -- vrátí se vyfiltrované řádky s požadovanými sloupci
+    where
+        filterRows :: [ColumnName] -> [ColumnName] -> [[Value]] -> [[Value]]
+        filterRows _ _ [] = []
+        filterRows columns tableColumns (r:rs) = filteredRow : filterRows columns tableColumns rs
+            where filteredRow = filterColumns columns tableColumns r
+        
+        filterColumns :: [ColumnName] -> [ColumnName] -> [Value] -> [Value]
+        filterColumns _ [] _ = []
+        filterColumns _ _ [] = []
+        filterColumns columns (column : tableColumns) (value:rowValues)
+            | column `elem` columns = value : filterColumns columns tableColumns rowValues -- pokus je sloupec z tabulky požadovaný v SELECT, připoj jeho hodnotu do vyfiltrovaného řádku
+            | otherwise = filterColumns columns tableColumns rowValues -- jinak hledej požadované sloupečky dál
+
 printResult :: [[Value]] -> IO ()
 printResult [] = putStrLn "No results found."
 printResult rows = mapM_ (putStrLn . unwords) rows
 
 extractParenthesizedContent :: String -> String
-extractParenthesizedContent input =
-    let maybeOpenParen = dropWhile (/= '(') input
-    in if null maybeOpenParen
-       then error $ "No opening parenthesis found in input: " ++ input
-       else
-           let maybeCloseParen = takeWhile (/= ')') (tail maybeOpenParen)
-           in if null maybeCloseParen
-              then error $ "No closing parenthesis found after opening in input: " ++ input
-              else maybeCloseParen
+extractParenthesizedContent input
+    | null maybeOpenParen = error $ "No opening parenthesis found in input: " ++ input
+    | null maybeCloseParen = error $ "No closing parenthesis found after opening in input: " ++ input
+    | otherwise = maybeCloseParen
+    where
+        maybeOpenParen = dropWhile (/= '(') input
+        maybeCloseParen = takeWhile (/= ')') (tail maybeOpenParen)
 
 parseSQL :: String -> SQLStatement
 parseSQL input = 
@@ -74,10 +103,6 @@ parseSQL input =
             let columns = parseColumns $ extractParenthesizedContent cols
             in Select columns tableName (parseJoin rest)
         _ -> error $ "Unknown command: " ++ input
-
-trim :: String -> String
-trim = f . f
-   where f = reverse . dropWhile isSpace
 
 -- Pomocná funkce pro parsování sloupců
 parseColumns :: String -> [ColumnName]
